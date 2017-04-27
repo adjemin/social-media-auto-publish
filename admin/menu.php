f0a36384bac2860cc5ff5850693e5cf3b6a44741
Enrico204 <enrico204@gmail.com>
2017-04-27 21:09:45 +0200

    Republish

--- admin/menu.php
+++ admin/menu.php
@@ -21,6 +21,8 @@ function xyz_smap_menu()
 	$page=add_submenu_page('social-media-auto-publish-settings', 'Social Media Auto Publish - Manage settings', ' Settings', 'manage_options', 'social-media-auto-publish-settings' ,'xyz_smap_settings');
 	add_submenu_page('social-media-auto-publish-settings', 'Social Media Auto Publish - Logs', 'Logs', 'manage_options', 'social-media-auto-publish-log' ,'xyz_smap_logs'); 
 	add_submenu_page('social-media-auto-publish-settings', 'Social Media Auto Publish - About', 'About', 'manage_options', 'social-media-auto-publish-about' ,'xyz_smap_about');
+
+	add_submenu_page('social-media-auto-publish-settings', 'Social Media Auto Publish - Republish', 'Republish', 'manage_options', 'social-media-auto-publish-re' ,'xyz_smap_republish');
 }
 
 
@@ -36,7 +38,33 @@ function xyz_smap_settings()
 	require( dirname( __FILE__ ) . '/footer.php' );
 }
 
+function xyz_smap_republish()
+{
+    if (isset($_GET["w"])) {
+        switch($_GET["w"]) {
+            case "f": // Facebook
+                $_POST['xyz_smap_post_permission'] = 1;
+                $_POST['xyz_smap_twpost_permission'] = 0;
+                $_POST['xyz_smap_lnpost_permission'] = 0;
+                xyz_link_publish(intval($_GET["id"]));
+                header("Location: admin.php?page=social-media-auto-publish-re&ok=1");
+                return;
+            case "t": // Twitter
+                $_POST['xyz_smap_post_permission'] = 0;
+                $_POST['xyz_smap_twpost_permission'] = 1;
+                $_POST['xyz_smap_lnpost_permission'] = 0;
+                xyz_link_publish(intval($_GET["id"]));
+                header("Location: admin.php?page=social-media-auto-publish-re&ok=1");
+                return;
+            default:
+                break;
+        }
+    }
 
+    require( dirname( __FILE__ ) . '/header.php' );
+    require( dirname( __FILE__ ) . '/republish.php' );
+    require( dirname( __FILE__ ) . '/footer.php' );
+}
 
 function xyz_smap_about()
 {
---
+++ admin/republish.php
@@ -0,0 +1,32 @@
+<h1>Republish</h1>
+
+<?php
+if (isset($_GET["ok"])) {
+    echo "<h2>Ripostato. Controllare.</h2>";
+}
+?>
+
+<table width="100%">
+    <tr>
+        <th>ID</th>
+        <th>Titolo</th>
+        <th>Azioni</th>
+    </tr>
+    <?php
+
+    foreach(get_posts() as $post) {
+        ?>
+        <tr>
+            <td><?= $post->ID ?></td>
+            <td><?= $post->post_title ?></td>
+            <td>
+                <button onclick="location.href='?page=social-media-auto-publish-re&w=f&id=<?= $post->ID ?>'">Riposta su Facebook</button>
+                ::
+                <button onclick="location.href='?page=social-media-auto-publish-re&w=t&id=<?= $post->ID ?>'">Riposta su Twitter</button>
+            </td>
+        </tr>
+        <?php
+    }
+
+    ?>
+</table>
